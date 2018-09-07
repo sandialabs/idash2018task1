06055f7928df3e278daa8315590b6af5fb549e94
Corey Hudson <cmhudso@sandia.gov>
2018-09-07 12:46:08 -0500

    Production versions

--- log_to_blockchain.py
+++ log_to_blockchain.py
@@ -1,54 +1,137 @@
 #TODO: thread to balance optimize disk i/o vs network, try to saturate
 #TODO: look into the rawtransaction way of doing multiple things at once
 #TODO: look into optimizing/issuing network directly, instead of through RPC
-import subprocess
+#import subprocess
 import binascii
+import math
 import json
-import sys
 import time
 import random
+import argparse
 import os
+import sys
 
-devnull = open(os.devnull, 'w')
+from Savoir import Savoir
 
-start_time = time.time()
-
-addr = json.loads(subprocess.check_output(["multichain-cli",\
-                                           "logchain","listaddresses"],\
-                                          stderr=devnull))[0]['address']	
-numinsertions = 0
-d = {}
-for line in sys.stdin.readlines():
-	(d['Timestamp'],d['Node'],d['ID'],d['Ref-ID'],d['User'],\
-         d['Activity'],d['Resource']) = line[:-1].split('\t')
-	for int_key in ['Node', 'ID', 'Timestamp', 'User', 'Ref-ID']:
+def insert(api,trps):
+ start_time = time.time()
+ delta = 1.0/float(trps)
+ target_time = None
+ behindct = ontimect = 0
+ cmd_times = []
+ addr = api.listaddresses()[0]['address']
+ #addr = json.loads(subprocess.check_output(["multichain-cli",\
+ #                                          sys.argv[1],"listaddresses"],\
+ #                                         stderr=devnull))[0]['address']	
+ numinsertions = 0
+ d = {}
+ for line in sys.stdin.readlines():
+	(d['timestamp'],d['node'],d['id'],d['ref-id'],d['user'],\
+         d['activity'],d['resource']) = line[:-1].split('\t')
+	for int_key in ['node', 'id', 'timestamp', 'user', 'ref-id']:
 		d[int_key] = int(d[int_key])
         sends = []
 
-        sends.append({'for':'logdata', 'key':'UID_' + str(d['Node']) + \
-                      '_' + str(d['ID']), 'data': binascii.hexlify(json.dumps(d))})
+        sends.append({'for':'logdata', 'key':'UID_' + str(d['node']) + \
+                      '_' + str(d['id']), 'data': binascii.hexlify(json.dumps(d))})
 	numinsertions += 1
 
 	for (k,v) in d.iteritems():
 
                 sends.append({'for':str(k), 'key':str(k)+'_'+str(v), \
-                              'data': binascii.hexlify('UID_' + str(d['Node']) + \
-                                                       '_' + str(d['ID']))})
-		if k == 'Timestamp':
-                        sends.append({'for':"node%dtimestamps" % int(d['Node']), \
+                              'data': binascii.hexlify('UID_' + str(d['node']) + \
+                                                       '_' + str(d['id']))})
+		if k == 'timestamp':
+                        sends.append({'for':"node%dtimestamps" % int(d['node']), \
                                       'key':str(v), \
                                       'data':binascii.hexlify('UID_' \
-                                                              + str(d['Node'])\
-                                                              + '_' + str(d['ID']))})
-        cmd = ['multichain-cli', 'logchain', 'createrawsendfrom', \
-               '%s' % addr, "{}", "%s" % json.dumps(sends), 'send']
+                                                              + str(d['node'])\
+                                                              + '_' + str(d['id']))})
+        #cmd = ['multichain-cli', sys.argv[1], 'createrawsendfrom', \
+        #       '%s' % addr, "{}", "%s" % json.dumps(sends), 'send']
 
-	subprocess.check_output(cmd,stderr=devnull)	
+	if target_time == None:
+		target_time = time.time()
+	if time.time() >= target_time:
+		behindct += 1
+		print 'fallen behind :-( (%d+%d=%d)' % (behindct,ontimect,behindct+ontimect)
+	else:
+		ontimect += 1
+		print 'caught back up :-) (%d+%d=%d)' % (behindct,ontimect,behindct+ontimect)
+	while time.time() < target_time:
+		pass
+	cmd_start = time.time()
+	#subprocess.check_output(cmd,stderr=devnull)
+	api.createrawsendfrom('%s' % addr, {}, sends, 'send')
+	cmd_time = time.time()-cmd_start
+	cmd_times.append(cmd_time)
+	target_time += delta
 
-elapsed_time = time.time() - start_time
+ elapsed_time = time.time() - start_time
 
-insertionstring = 'insertion took %s seconds, %s secs/record (%s records)' % \
+ insertionstring = 'insertion took %s seconds, %s secs/record (%s records)' % \
 			(elapsed_time, elapsed_time/numinsertions, numinsertions)
 
-print insertionstring
+ print insertionstring
+ histogram = map(lambda x:0, range(40))
+ bigger = 0
+ maxval = 0
+ for sample in cmd_times:
+	if sample > maxval:
+		maxval = sample
+	bucket = int(math.floor(sample/.1))
+	if bucket > 39:
+		bigger += 1
+	else:
+		histogram[bucket] += 1
+ print histogram
+ print bigger
+ print maxval
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--rpcuser', \
+                        action='store', \
+                        help='multichain rpc user')
+    parser.add_argument('--rpcport', \
+                        action='store', \
+                        help='multichain rpc port')
+    parser.add_argument('--rpcpasswd', \
+                        action='store', \
+                        help='multichain rpc password')
+    parser.add_argument('--chainname', \
+                        action='store', \
+                        help='multichain chain name')
+    parser.add_argument('--targetrecordspersecond', \
+                        action='store', \
+                        type=int, \
+                        help='attempt to insert at this rate')
+
+    args = parser.parse_args()
+    if args.rpcuser:
+        rpcuser = args.rpcuser
+    else:
+        rpcuser = 'multichainrpc'
+    if args.rpcpasswd:
+        rpcpasswd = args.rpcpasswd
+    else:
+        filename = '/home/ubuntu/.multichain/logchain/multichain.conf'
+        rpcpasswd = dict(map(lambda y: y.split('='), \
+                     filter(lambda x: x, \
+                            open(filename).read().split('\n'))))['rpcpassword']
+    rpchost = 'localhost'
+
+    if args.rpcport:
+        rpcport = args.rpcport
+    else:
+        rpcport = '4920'
+
+    if args.chainname:
+        chainname = args.chainname
+    else:
+        chainname = 'logchain'
+
+    #print 'connecting Savoir(%s,%s,%s,%s,%s)' % (rpcuser,rpcpasswd,rpchost,rpcport,chainname)
+    savoirapi = Savoir(rpcuser,rpcpasswd,rpchost,rpcport,chainname)
+    insert(savoirapi,args.targetrecordspersecond)
 
--- query_blockchain.py
+++ query_blockchain.py
@@ -9,15 +9,6 @@ import bisect
 from multiprocessing import Pool
 
 from Savoir import Savoir
-rpcuser = 'multichainrpc'
-filename = '/home/ubuntu/.multichain/logchain/multichain.conf'
-rpcpasswd = dict(map(lambda y: y.split('='), \
-                     filter(lambda x: x, \
-                            open(filename).read().split('\n'))))['rpcpassword']
-rpchost = 'localhost'
-rpcport = '4920'
-chainname = 'logchain'
-api = Savoir(rpcuser,rpcpasswd,rpchost,rpcport,chainname)
 
 #wrapping calls to the timestamp stream in an interator lets us
 #easily binary search in them using python's bisect 
@@ -29,7 +20,7 @@ class TimestampIterator:
         global api
         self.nodenum = nodenum
         self.api = api
-        self.numrecords = self.api.liststreams("node%dtimestamps" % self.nodenum)[0]['keys']
+        self.numrecords = self.api.liststreams("node%dtimestamps" % self.nodenum)[0]['items']
         self.recordnum = 0
 
     def __iter__(self):
@@ -39,14 +30,14 @@ class TimestampIterator:
         return self.numrecords
 
     def __getitem__(self,i):
-        return int(self.api.liststreamkeys("node%dtimestamps" % self.nodenum, '*', False, 1, i, True)[0]['key'])
+        return int(self.api.liststreamkeyitems("node%dtimestamps" % self.nodenum, '*', False, 1, i, True)[0]['key'])
             
     def next(self):
         if self.recordnum == self.numrecords:
             raise StopIteration
         else:
             self.recordnum += 1
-            return int(self.api.liststreamkeys( "node%dtimestamps" % self.nodenum, '*', False, 1, self.recordnum-1, True)[0]['key'])
+            return int(self.api.liststreamkeyitems( "node%dtimestamps" % self.nodenum, '*', False, 1, self.recordnum-1, True)[0]['key'])
 
 
 #===== binary search for timestmap ranges =====
@@ -56,36 +47,43 @@ def interval_ids(nodenum, starttimestamp, endtimestamp):
 
         lower_bound = bisect.bisect_left(TimestampIterator(nodenum), int(starttimestamp))
         upper_bound = bisect.bisect_right(TimestampIterator(nodenum), int(endtimestamp), lo=lower_bound)
+        #print 'nodenum %d, lower %s' % (nodenum, lower_bound)
+        #print 'nodenum %d, upper %s' % (nodenum, upper_bound)
 
         if upper_bound - lower_bound == 0:
             return set([])
         else:
             resulti = binascii.unhexlify(api.liststreamkeyitems("node%dtimestamps" % nodenum,\
                                                                 "*", False, 1, lower_bound, True)[0]['data'])
-
             resultj = binascii.unhexlify(api.liststreamkeyitems("node%dtimestamps" % nodenum,\
-                                                                "*", False, 1, upper_bound-1, True)[0]['data'])
-            
-            return set(map(lambda x: 'UID_%d_%d' % (nodenum,x), \
-                           range(int(resulti.split('_')[2]),int(resultj.split('_')[2])+1)))
+                                                                "*", False, 1, upper_bound-1, True)[0]['data'])            
+
+            return {'UID_%d_%d' % (nodenum, x) for x in xrange(int(resulti.split('_')[2]), int(resultj.split('_')[2])+1)}
 
 #==== main query logic ====
 
 def loaditem(itemid):
     global api
-    return json.loads(binascii.unhexlify(api.liststreamkeyitems("logdata", '%s' % itemid)[0]['data']))
+    answer = api.liststreamkeyitems("logdata", '%s' % itemid)
+    if len(answer) > 0:
+        assert len(answer) == 1
+        return json.loads(binascii.unhexlify(answer[0]['data']))
+    else:
+        return None
 
 def print_result(d):
-        print('%s\t%d\t%d\t%d\t%d\t%s\t%s' % (d['Timestamp'], \
-                                              d['Node'], \
-                                              d['ID'], \
-                                              d['Ref-ID'], \
-                                              d['User'], \
-                                              d['Activity'], \
-                                              d['Resource']))
+    if d:
+        print('%s\t%d\t%d\t%d\t%d\t%s\t%s' % (d['timestamp'], \
+                                              d['node'], \
+                                              d['id'], \
+                                              d['ref-id'], \
+                                              d['user'], \
+                                              d['activity'], \
+                                              d['resource']))
 
 
 def check_constraints(filters,d):
+    if d:
         for (cnt,kv) in filters:
                 k = kv.split('_')[0]
                 v = reduce(lambda x,y: x+'_'+y, kv.split('_')[1:])
@@ -96,6 +94,8 @@ def check_constraints(filters,d):
                 if not d[k] == v:
                         return False
         return d
+    else:
+        return False
 
 #clauses is a dictionary 
 #timestamp range is a tuple or None
@@ -110,8 +110,8 @@ def query(clauses, timestamp_range, sortby, descendingp):
     #if there's a timestamp constraint, get the keys, this should be logn
     timestamp_matches = set([])
     if timestamp_range:
-        if 'Node' in clauses:
-            timestamp_matches = timestamp_matches.union(interval_ids(int(clauses['Node']), \
+        if 'node' in clauses:
+            timestamp_matches = timestamp_matches.union(interval_ids(int(clauses['node']), \
                                                                      timestamp_range[0], \
                                                                      timestamp_range[1]))
         else:
@@ -145,15 +145,14 @@ def query(clauses, timestamp_range, sortby, descendingp):
             if timestamp_range and (len(timestamp_matches) <= match_cts[0][0]):
                     matches = timestamp_matches
             else:
-                    matches = set(map(lambda x: binascii.unhexlify(x['data']), \
-                                      api.liststreamkeyitems( match_cts[0][1].split('_')[0], \
-                                                              '%s' % match_cts[0][1], False, \
-                                                              pow(2,30))))
+                    matches = {binascii.unhexlify(x['data']) for x in api.liststreamkeyitems( match_cts[0][1].split('_')[0], \
+                                                                                              '%s' % match_cts[0][1], False, \
+                                                                                              pow(2,30))}
                     if timestamp_range:
                             matches = matches.intersection(timestamp_matches)
 
     elapsed_time = time.time() - start_time
-    print 'took %s seconds to identify %s potential records' % (elapsed_time, len(matches))
+    #print 'took %s seconds to identify %s potential records' % (elapsed_time, len(matches))
 
     start_time = time.time()
 
@@ -181,7 +180,7 @@ def query(clauses, timestamp_range, sortby, descendingp):
 
     elapsed_time = time.time() - start_time
 
-    print 'took %s seconds to return %s records' % (elapsed_time, len(actual_results))
+    #print 'took %s seconds to return %s records' % (elapsed_time, len(actual_results))
 
 
 #argparse based cmdline
@@ -189,6 +188,18 @@ def query(clauses, timestamp_range, sortby, descendingp):
 #--timestamplow value --timestamphigh --sortby Colname3 --reverse-sort
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--rpcuser', \
+                        action='store', \
+                        help='multichain rpc user')
+    parser.add_argument('--rpcport', \
+                        action='store', \
+                        help='multichain rpc port')
+    parser.add_argument('--rpcpasswd', \
+                        action='store', \
+                        help='multichain rpc password')
+    parser.add_argument('--chainname', \
+                        action='store', \
+                        help='multichain chain name')
     parser.add_argument('--clause', \
                         action='append', \
                         help='a constraint in the form of COLNAME=VALUE, e.g. Node=3, can appear multiple times and will be ANDed')
@@ -211,12 +222,38 @@ if __name__=='__main__':
                         help='sort in descending order (default is ascending)')
     args = parser.parse_args()
 
+    if args.rpcuser:
+        rpcuser = args.rpcuser
+    else:
+        rpcuser = 'multichainrpc'
+    if args.rpcpasswd:
+        rpcpasswd = args.rpcpasswd
+    else:
+        filename = '/home/ubuntu/.multichain/logchain/multichain.conf'
+        rpcpasswd = dict(map(lambda y: y.split('='), \
+                     filter(lambda x: x, \
+                            open(filename).read().split('\n'))))['rpcpassword']
+    rpchost = 'localhost'
+
+    if args.rpcport:
+        rpcport = args.rpcport
+    else:
+        rpcport = '4920'
+
+    if args.chainname:
+        chainname = args.chainname
+    else:
+        chainname = 'logchain'
+
+    #print 'connecting Savoir(%s,%s,%s,%s,%s)' % (rpcuser,rpcpasswd,rpchost,rpcport,chainname)
+    api = Savoir(rpcuser,rpcpasswd,rpchost,rpcport,chainname)
+
     clauses = {}
     if args.clause:
         for clause in args.clause:
             (k,v) = clause.split('=')
             #TODO: check to make sure well formed
-            clauses[k] = v
+            clauses[k.lower()] = v
 
     timestamprange = None
     if args.timestamplow or args.timestamphigh:
@@ -226,5 +263,5 @@ if __name__=='__main__':
         if args.timestamphigh:
             timestamprange = (timestamprange[0], int(args.timestamphigh))
 
-    query(clauses, timestamprange, args.sortby, args.descendingp)
+    query(clauses, timestamprange, args.sortby.lower() if args.sortby else None, args.descendingp)
     
